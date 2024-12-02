3f2509ea4982e36a6a08386957a1024d3170cc12
Swapnil ingle <ingleswapnil2004@gmail.com>
2024-12-02 23:11:42 +0530

    cors err update

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ const port = 3002;
 // Middleware configuration
 app.use(cors(
     {
-        origin: [
-            '*',
-        ],
+        origin: "*" ,
         optionsSuccessStatus: 200  
     }
 ));
